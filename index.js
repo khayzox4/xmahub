b3b34260e3334aa91282f782766d65e4d52ee7f7
khayzox4 <kkayzox4@gmail.com>
2026-04-16 15:29:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1963,7 +1963,7 @@ if (
   if (!guild) return;
 
   try {
-    await interaction.reply({ content, ephemeral: true });
+    await interaction.reply({ content, flags: 64 });
 
     console.log("NSFW click:", interaction.customId);
     console.log("ROLE_NUDE1_ID =", ROLE_NUDE1_ID);
